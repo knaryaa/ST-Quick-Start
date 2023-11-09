a0ec1a10ef3e6149a4a5af2dfda27750c534f780
Yalcin Taf <knarya07@gmail.com>
2023-11-09 19:36:40 +0300

    Final Version

---
+++ Source/ST_QuickStart/MassAITest.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MassStateTreeTypes.h"
+
+
+
+class ST_QUICKSTART_API MassAITest
+{
+public:
+	MassAITest();
+	~MassAITest();
+};
---
+++ Source/ST_QuickStart/ST_QuickStart.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ST_QuickStart.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ST_QuickStart, "ST_QuickStart" );
